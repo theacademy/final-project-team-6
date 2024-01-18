b87418f68c43d4a27a6d71847ef141a3d9df33c8
IsmailTBaysal <i.timucinbaysal@gmail.com>
2024-01-18 10:29:54 -0600

    appointment dto completed

--- src/main/java/com/example/hasapp/dto/Appointment.java
+++ src/main/java/com/example/hasapp/dto/Appointment.java
@@ -1,4 +1,61 @@
 package com.example.hasapp.dto;
 
+
+import java.time.LocalDateTime;
+
 public class Appointment {
+    private int appointmentId;
+    private int patientId;
+    private int doctorId;
+    private LocalDateTime appointmentDateTime;
+    private String purpose;
+    private String status;
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public LocalDateTime getAppointmentDateTime() {
+        return appointmentDateTime;
+    }
+
+    public void setAppointmentDateTime(LocalDateTime appointmentDateTime) {
+        this.appointmentDateTime = appointmentDateTime;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
