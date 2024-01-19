b46ebc6f08020f1ce743f2aba0a89ff4541ad8e2
jahedprince <jahedprince183@gmail.com>
2024-01-19 05:06:46 -0500

    edited doctor service impl

--- src/main/java/com/example/hasapp/service/DoctorServiceImpl.java
+++ src/main/java/com/example/hasapp/service/DoctorServiceImpl.java
@@ -2,8 +2,8 @@ package com.example.hasapp.service;
 
 import com.example.hasapp.dao.DoctorRepo;
 import com.example.hasapp.dto.Doctor;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,97 +11,58 @@ import java.util.List;
 @Service
 public class DoctorServiceImpl implements DoctorServiceInterface {
 
-    @Autowired
-    private DoctorRepo doctorDao;
+    private final DoctorRepo doctorDao;
 
+    @Autowired
     public DoctorServiceImpl(DoctorRepo doctorDao) {
         this.doctorDao = doctorDao;
     }
 
     @Override
     public List<Doctor> getAllDoctors() {
-        // get a list of all doctors
         return doctorDao.getAllDoctors();
     }
 
     @Override
     public Doctor getDoctorById(int id) {
-        Doctor doctor;
-        String notFoundMsg = "Doctor Not Found.";
-
-        try {
-            // find doctors by id
-            doctor = doctorDao.getDoctorById(id);
-        } catch (DataAccessException e) {
-            // if a DataAccessException occurs,
-            // set a not found message
-            doctor = new Doctor();
-            doctor.setDFName(notFoundMsg);
-            doctor.setDLName(notFoundMsg);
-        }
-
-        return doctor;
+        return doctorDao.getDoctorById(id);
     }
 
     @Override
     public Doctor addNewDoctor(Doctor doctor) {
-        String notAddedMsg = " blank, doctor NOT added";
-
-        // if the doctor's first or last name is blank,
-        // set a message in the first name and/or last name
-        // that the doctor was not added
-        if (doctor.getDFName().isBlank() || doctor.getDLName().isBlank()) {
-            if (doctor.getDFName().isBlank()) {
-                doctor.setDFName("First Name" + notAddedMsg);
-            }
-            if (doctor.getDLName().isBlank()) {
-                doctor.setDLName("Last Name" + notAddedMsg);
-            }
-            return doctor;
-        }
-
-        // add the doctor
-        Doctor doctorAdded = doctorDao.addDoctor(doctor);
-
-        // if the doctor was successfully added
-        if (doctorAdded != null) {
-            return doctorAdded;
-        } else {
-            return null;
-        }
+        validateDoctorData(doctor);
+        return doctorDao.addDoctor(doctor);
     }
 
     @Override
     public Doctor updateDoctorData(int id, Doctor doctor) {
-        String nonMatchingIdsMsg = "IDs do not match, doctor not updated";
-
-        try {
-            // if the path variable id and Doctor object id are not the same
-            // set a message stating that the doctor was not updated
-            if (id != doctor.getDid()) {
-                doctor.setDFName(nonMatchingIdsMsg);
-                doctor.setDLName(nonMatchingIdsMsg);
-                return doctor;
-            }
-
-            // check if the doctor exists
-            Doctor doctorFound = getDoctorById(id);
-
-            // if the doctor was found
-            if (doctorFound.getDid() != 0) {
-                // update doctor
-                doctorDao.updateDoctor(doctor);
-                return doctor;
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            return null;
+        validateDoctorData(doctor);
+        if (id == doctor.getDid()) {
+            doctorDao.updateDoctor(doctor);
+        } else {
+            throw new IllegalArgumentException("Mismatched doctor ID");
         }
+        return doctor;
     }
 
     @Override
     public void deleteDoctorById(int id) {
         doctorDao.deleteDoctor(id);
+    }
+
+    private void validateDoctorData(Doctor doctor) throws IllegalArgumentException {
+        if (doctor == null) {
+            throw new ValidationException("Doctor cannot be null");
+        }
+        if (doctor.getDFName() == null || doctor.getDFName().isBlank()) {
+            throw new ValidationException("Doctor's first name is required.");
+        }
+        if (doctor.getDLName() == null || doctor.getDLName().isBlank()) {
+            throw new ValidationException("Doctor's last name is required.");
+        }
+        if (doctor.getSpecialty() == null || doctor.getSpecialty().isBlank()) {
+            throw new ValidationException("Doctor's specialty is required.");
+        }
+
     }
 }
