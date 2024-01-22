d92fbec4d0b527fa4e8ca9c023026d85adf4bd24
Luan Nguyen <d8ll8s@gmail.com>
2024-01-22 13:54:39 -0500

    FrontEnd patient added, not finished

--- frontend/src/Components/DashboardComponents/patientBox.js
+++ frontend/src/Components/DashboardComponents/patientBox.js
@@ -1,18 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Typography, Paper, Grid, Divider } from "@mui/material";
 
-const PatientBox = ({ patients }) => {
+const PatientBox = () => {
+  const [patients, setPatients] = useState([]);
+
+  // Fetch patients from backend
+  const fetchPatients = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/patients");
+      if (!response.ok) {
+        throw new Error("Failed to fetch patients");
+      }
+      const data = await response.json();
+      setPatients(data);
+    } catch (error) {
+      console.error("Error fetching patients:", error.message);
+    }
+  };
+
+  // Call fetchPatients when the component mounts
+  useEffect(() => {
+    fetchPatients();
+  }, []);
+
   return (
-    <div>
-      <h2>Patients</h2>
-      <ul>
-        {patients.map((patient) => (
-          <li key={patient.PID}>
-            {patient.pFName} {patient.pLName}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Paper
+      elevation={3}
+      style={{ padding: "16px", marginTop: "16px", backgroundColor: "#fbfaf8" }}
+    >
+      <Typography variant="h6" gutterBottom>
+        Patients
+      </Typography>
+      <Divider style={{ marginBottom: "16px" }} />
+      <div style={{ maxHeight: "200px", overflowY: "auto" }}>
+        <Grid container spacing={2}>
+          {patients.map((patient, index) => (
+            <Grid item xs={12} key={patient.pid}>
+              <Paper
+                elevation={2}
+                style={{
+                  padding: "16px",
+                  borderRadius: "8px",
+                  textAlign: "left",
+                  backgroundColor: "white",
+
+                  marginBottom: "1px",
+                }}
+              >
+                <Typography variant="subtitle1" gutterBottom>
+                  {`${patient.pFName} ${patient.pLName}`}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  {patient.phoneNumber}
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Paper>
   );
 };
-
 export default PatientBox;
