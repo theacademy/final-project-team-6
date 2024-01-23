361cf3bf5b086aa963c8c023ebb797ae9f5d195a
IsmailTBaysal <i.timucinbaysal@gmail.com>
2024-01-23 12:08:50 -0600

    still working on auth

---
+++ frontend/src/Components/Auth/AuthContext.js
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useState } from "react";
+import { getCurrentUser } from "./api";
+import { useEffect } from "react";
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem("jwt_token")
+  );
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const login = async () => {
+    setIsAuthenticated(true);
+    try {
+      const user = await getCurrentUser();
+      setCurrentUser(user);
+    } catch (error) {
+      console.error("Failed to fetch current user:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      login();
+    } else {
+      setCurrentUser(null);
+    }
+  }, [isAuthenticated]);
+
+  const logout = () => {
+    localStorage.removeItem("jwt_token");
+    setIsAuthenticated(false);
+  };
+
+  const contextValue = {
+    currentUser,
+    isAuthenticated,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
