13e980f098aee11fa5f7ee1bca0632965c46f825
Luan Nguyen <d8ll8s@gmail.com>
2024-01-23 13:16:18 -0500

    table pagination

--- frontend/src/Components/PatientComponent/PatientManagement.js
+++ frontend/src/Components/PatientComponent/PatientManagement.js
@@ -22,6 +22,76 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 
+import { useTheme } from "@mui/material/styles";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
 const PatientManagement = () => {
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -97,7 +167,7 @@ const PatientManagement = () => {
 
   const handleAddPatient = async () => {
     try {
-      const response = await fetch("http://localhost:8080/patient/add", {
+      const response = await fetch("http://localhost:8080/addPatient", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -116,7 +186,9 @@ const PatientManagement = () => {
       setNewPatient({
         pFName: "",
         pLName: "",
-        specialty: "",
+        birthday: "",
+        phoneNumber: "",
+        insurancePatient: "",
       });
     } catch (error) {
       console.error("Error adding patient:", error.message);
@@ -186,8 +258,26 @@ const PatientManagement = () => {
     setNewPatient({
       pFName: "",
       pLName: "",
-      specialty: "",
+      birthday: "",
+      phoneNumber: "",
+      insurancePatient: "",
     });
+  };
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - patients.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   return (
@@ -226,7 +316,13 @@ const PatientManagement = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {patients.map((patient) => (
+            {(rowsPerPage > 0
+              ? patients.slice(
+                  page * rowsPerPage,
+                  page * rowsPerPage + rowsPerPage
+                )
+              : patients
+            ).map((patient) => (
               <StyledTableRow key={patient.PID}>
                 <StyledTableCell component="th" scope="row">
                   {patient.pFName}
@@ -268,6 +364,27 @@ const PatientManagement = () => {
               </StyledTableRow>
             ))}
           </TableBody>
+
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={5}
+                count={patients.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
         </Table>
       </TableContainer>
 
@@ -392,14 +509,38 @@ const PatientManagement = () => {
             margin="normal"
           />
           <TextField
-            label="Specialty"
-            value={newPatient.specialty}
+            label="Phone Number"
+            value={newPatient.phoneNumber}
             onChange={(e) =>
-              setNewPatient({ ...newPatient, specialty: e.target.value })
+              setNewPatient({ ...newPatient, phoneNumber: e.target.value })
             }
             fullWidth
             margin="normal"
           />
+
+          <TextField
+            label="Birthday"
+            value={newPatient.birthday}
+            onChange={(e) =>
+              setNewPatient({ ...newPatient, birthday: e.target.value })
+            }
+            fullWidth
+            margin="normal"
+          />
+
+          <TextField
+            label="InsuranceProvider"
+            value={newPatient.insuranceProvider}
+            onChange={(e) =>
+              setNewPatient({
+                ...newPatient,
+                insuranceProvider: e.target.value,
+              })
+            }
+            fullWidth
+            margin="normal"
+          />
+
           <Button
             variant="contained"
             color="primary"
